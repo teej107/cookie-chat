87520702510df607b5e6e46ae6579fd96221ef0a
teej107 <teej107@gmail.com>
2017-02-14 21:01:55 -0700

    JavaScript Done! Some HTML/CSS/Angular finished

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,55 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular.module('chatroom').controller('mainCtrl', function ($scope, messageService)
+{
+    //The getMessages function will call the getMessages method on the messageService object.
+    //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    $scope.getMessages = function (log)
+    {
+        messageService.getMessages().then(function (success)
+        {
+            $scope.messages = success.data;
+            $scope.messages.forEach(function (message, i)
+            {
+                message.color = i % 2 === 0 ? 'blue-message' : 'gray-message'
+            });
+            if (log)
+            {
+                console.log($scope.messages);
+            }
+        });
+    };
+    $scope.getMessages(true);
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+    //pass that text to the postMessage method on the messageService object which will
+    //then post it to the backend.
+    $scope.postMessage = function (message)
+    {
+        if(message.trim() === 'DELETE')
+        {
+            messageService.deleteMessage();
+        }
+        else
+        {
+            $scope.messages.push({message: 'posting message...'});
+            messageService.postMessage(message).then(function (success)
+            {
+                $scope.getMessages();
+            });
+        }
+        $scope.message = '';
+    };
 
+    $scope.timestampToDate = function (messageObj)
+    {
+        return messageService.timestampToDate(messageObj.createdAt);
+    }
 
+    //uncomment this code when your getMessages function is finished
+    //This goes and gets new data every second, which mimicking a chat room experience.
+    setInterval(function ()
+    {
+        $scope.getMessages();
 
-
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
-
-
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+    }, 1500)
 
 })
--- js/messageService.js
+++ js/messageService.js
@@ -1,19 +1,46 @@
-angular.module('chatroom').service('messageService', function($http){
-  //Here you'll need to create two methods. One called postMessage and the other called getMessages.
+angular.module('chatroom').service('messageService', function ($http)
+{
+    //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
-  //On the lines below create a getMessages method. This method will retrieve data from the backend.
-  //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
-  //Be sure to return whatever gets returned from $http so you can call .then in your controller.
-
-
-
-
-  //On the line below create the postMessage method. This method will add data to the backend server.
-  //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
-  //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
-
-
+    //On the lines below create a getMessages method. This method will retrieve data from the backend.
+    //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
+    //Be sure to return whatever gets returned from $http so you can call .then in your controller.
+    this.getMessages = function ()
+    {
+        return $http({
+            method: 'GET',
+            url: 'https://practiceapi.devmounta.in/api/chats'
+        });
+    };
 
 
+    //On the line below create the postMessage method. This method will add data to the backend server.
+    //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
+    //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
+    //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
+    this.postMessage = function (myMessage)
+    {
+        return $http({
+            method: 'POST',
+            url: 'https://practiceapi.devmounta.in/api/chats',
+            data: {message: myMessage}
+        });
+    };
+
+    this.deleteMessage = function (index)
+    {
+        return $http({
+            method: 'DELETE',
+            url: 'https://practiceapi.devmounta.in/api/chats'
+        });
+    };
+
+    this.timestampToDate = function (timestamp)
+    {
+        var date = new Date(timestamp);
+        var pm = date.getHours() > 12;
+        var hour = (pm ? date.getHours() - 12 : date.getHours());
+        var minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        return hour + ":" + minute + (pm ? " PM" : " AM") + " | " + (date.getMonth() + 1) + "/" + date.getDay() + "/" + date.getYear();
+    }
 });
